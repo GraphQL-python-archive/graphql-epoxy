7887a5d02ad03b1d0a16711be0d68b1dc9962086
Jake <me@jh.gg>
2015-10-26 15:24:53 -0400

    Fix flake8 error.

--- epoxy/metaclasses/object_type.py
+++ epoxy/metaclasses/object_type.py
@@ -58,7 +58,7 @@ class ObjectTypeMeta(type):
         known_interface_resolvers = {}
 
         for interface in interfaces:
-            interface.get_fields() # This triggers the interface to resolve the field thunks.
+            interface.get_fields()  # This triggers the interface to resolve the field thunks.
             fields += registry._get_interface_declared_fields(interface)
 
         for field_attr_name, field in fields:
